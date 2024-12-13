5bd0262fa3138d6d75d708ce07ad80f6fdd49dfb
Cas Li <cyli@college.harvard.edu>
2024-12-12 21:14:35 -0500

    fix bug

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ roleSelector.onchange = (event) => {
   socket.send(JSON.stringify({ type: 'role', role }));
 };
 
-const socket = new WebSocket(Deno.env.get("WEBSOCKET_URL")!);
+const socket = new WebSocket(Deno.env.get("WEBSOCKET_URL"));
 
 socket.onopen = () => {
   console.log("WebSocket connection established.");
